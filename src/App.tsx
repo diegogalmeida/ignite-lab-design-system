16c471f87d071a9112dbdfaf745103d0bba2730c
Diego Almeida <diegogalmeida90@gmail.com>
2022-10-13 00:10:03 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-black mb-4">Hello Word</h1>
+
+      <button className="px-4 py-2 rounded text-black font-bold text-sm bg-cyan-500 hover:bg-cyan-300">
+        Enviar
+      </button>
+    </>
+  )
+}
