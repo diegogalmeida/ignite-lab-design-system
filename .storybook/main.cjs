167337a4ef395110673491b5f0514cee6cb18e60
Diego Almeida <57111300+diegogalmeida@users.noreply.github.com>
2022-10-13 13:53:22 -0300

    Update main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if ( configType === 'PRODUCTION' ) {
-      configBase = '/ignite-lab-design-system/'
+      config.base = '/ignite-lab-design-system/'
     }
 
     return config
